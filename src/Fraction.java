d22abb95344d21e11d1bf5c99980e1555e9763aa
Doug Ryan <djr1@users.noreply.github.com>
2021-08-27 21:35:33 -0700

    initial fraction template

---
+++ src/Fraction.java
@@ -0,0 +1,22 @@
+/**
+ * Allow the user the ability to enter two fractions. Add the two fractions
+ * without reducing the result. Output the inputted fractions and the result.
+ * The inputted fractions have one space before and after the /. The output
+ * has no spaces in each fraction.
+ *
+ * @author
+ *
+ */
+import java.util.Scanner;
+
+public class Fraction {
+  /**
+   * Driver for the fraction calculator
+   *
+   * @param args arguments entered from the command line
+   *
+   */
+  public static void main (String[] args) {
+
+  }
+}
